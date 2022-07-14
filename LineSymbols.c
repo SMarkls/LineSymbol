fa69d019c05a06066f3df4fb7d1815319ab55ba0
markls <isxakov.mark@gmail.com>
2022-07-14 18:05:19 +0500

    запилена работа с файлами

--- LineSymbols.c
+++ LineSymbols.c
@@ -8,18 +8,24 @@
 void main(void)
 {
 	setlocale(LC_ALL, "RUS");
-	char* line = "while A<>B do C:=Arr[$1F0 - 2];";
+	char line[256];
+	FILE* f;
+	f = fopen("INPUT.txt", "r");
+	fgets(line, 256, f);
+	fclose(f);
+	//char* line = "while A<>B do C:=Arr[$1F0 - 2];";
 	for (int i = 0; i < strlen(line); i++)
 	{
 		splitSymsClasses(line[i]);
 	}
-	
+
+	f = fopen("OUTPUT.txt", "w");
 
 	for (int i = 0; i < countSyms; i++)
 	{
 		if (strcmp(words[i].key, "др.символ") == 0)
 		{
-			printf("REJECTED");
+			fputs("REJECT", f);
 			return;
 		}
 		splitWordsClasses(i);
@@ -31,5 +37,5 @@ void main(void)
 	{
 		printf("['%s', '%s'] ", lex[i].word, lex[i].key);
 	}*/
-	printf("%s", Result());
+	fputs(Result(), f);
 }
